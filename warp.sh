92ad5ef49c28c70d550273e16b67cf56327624f7
O»K»I»L»A <89906412+o-k-l-l-a@users.noreply.github.com>
2025-12-14 10:37:49 -0800

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -4,7 +4,7 @@ echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] ht
 sudo apt-get update && sudo apt-get install -y cloudflare-warp
 
 printf "y\n" | warp-cli registration new
-
+warp-cli registration license c1l9y72z-V2c3Gk09-7A3Ft9K6
 warp-cli mode proxy
 warp-cli proxy port 4848
 warp-cli connect
